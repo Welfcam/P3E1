278aca5476d5601ecdeda1bf32a87a48c62738d0
Welfcam <welfcam@gmail.com>
2024-10-08 16:41:02 +0200

    Code comments

--- force-app/main/default/lwc/accountCasesSearcher/accountCasesSearcher.js
+++ force-app/main/default/lwc/accountCasesSearcher/accountCasesSearcher.js
@@ -1,6 +1,9 @@
+//Import de la classe Lightning element et des décorateurs
 import { LightningElement, track, api } from 'lwc';
+//Import de la méthode findCaseBySubject de AccountCaseController
 import findCasesBySubject from '@salesforce/apex/AccountCasesController.findCasesBySubject';
 
+//Détail des colonnes de la lightning datatable
 const COLUMNS = [
     { label: 'Subject', fieldName: 'Subject', type: 'text' },
     { label: 'Status', fieldName: 'Status', type: 'text' },
@@ -8,30 +11,42 @@ const COLUMNS = [
 ];
 
 export default class AccountCaseSearchComponent extends LightningElement {
+    //récupère la propriété publique recordId (ID de la page active)
     @api recordId;
+    //suit les propriétés de cases
     @track cases;
+    //suit les propriétés de error
     @track error;
+    //variable à laquelle sera affectée la valeur entrée par l'utilisateur
     searchTerm = '';
+    //variable à laquelle sera affecté le message d'information en cas de recherche sans résultat
     noMatch;
+    //variable columns utilisée dans le HTML
     columns = COLUMNS;
 
+    //permet de mettre à jour la valeur de searchTerm au moment de la saisie par l'utilsateur dans la barre de recherche
     updateSearchTerm(event) {
         this.searchTerm = event.target.value;
     }
 
+    //Gère ce qu'il se passe lorsque l'utilsateur clique sur le bouton Rechercher
     handleSearch() {
+        //Appelle la fonction Apex findCasesBySubject
         findCasesBySubject({ accountId: this.recordId, subjectSearchTerm: this.searchTerm })
             .then(result => {
+                //s'il y a au moins 1 résultat, les données sont affichées
                 if(result.length > 0) {
                     this.cases = result;
                     this.noMatch = undefined;
                     this.error = undefined;
+                //s'il n'ya a aucun résultat, une message d'info est affiché
                 } else {
                     this.noMatch = 'There are no cases matching your search.'
                     this.cases = undefined;
                     this.error = undefined;
                 }
             })
+            //S'il y a une erreur, un message d'erreur est affiché
             .catch(error => {
                 this.error = 'An error occurred while searching for cases.';
             });
--- force-app/main/default/lwc/accountOpportunitiesViewer/accountOpportunitiesViewer.js
+++ force-app/main/default/lwc/accountOpportunitiesViewer/accountOpportunitiesViewer.js
@@ -1,13 +1,22 @@
+//Import de la classe LightningElement et des décorateurs
 import { LightningElement, api, wire, track } from 'lwc';
+//Import de la méthode Apex getOpportunities
 import getOpportunities from '@salesforce/apex/AccountOpportunitiesController.getOpportunities';
+//Import de la méthode refreshApex
 import { refreshApex } from "@salesforce/apex";
 
 export default class AccountOpportunitiesViewer extends LightningElement {
+    //récupère la propriété publique recordId (ID de la page active)
     @api recordId;
+    //suit les propriétés de opportunities
     @track opportunities;
+    //suit les propriétés de error
     @track error; //= {};
+    //création d'une variable wiredOppotunitiesResult pour récupérer les données de getOpportunities (data et error)
     wiredOpportunitiesResult;
+    //création d'une variable pour afficher une message informatif lorsque getOpportunities ne renvoie aucun résultat
     emptyTable;
+    //définit la structure des colonnes de la lightning datatable (champs, noms et type des données à afficher)
     columns = [
         { label: 'Opportunity Name', fieldName: 'Name', type: 'text' },
         { label: 'Amount', fieldName: 'Amount', type: 'currency' },
@@ -15,26 +24,32 @@ export default class AccountOpportunitiesViewer extends LightningElement {
         { label: 'Stage', fieldName: 'StageName', type: 'text' }
     ];
 
-    @wire(getOpportunities, { accountId: '$recordId' }) 
+    //permet de récupérer les opportunités issues de la méthode Apex getOpportunities dont l'accountId correspond à l'ID du compte actif
+    @wire(getOpportunities, { accountId: '$recordId' })
+    //la fonction wiredOpportunities récupère en paramètre les résultas de la fonction Apex getOpportunities
     wiredOpportunities (result) {
+        //Affecte le résultat de la fonction Apex getOpporutinites à la viariable wiredOpportunitiesResult
         this.wiredOpportunitiesResult = result;
+        //Si les données de result sont >0 (au moins une opportunité), les opportinités sont affichées
         if (result.data) {
             if(result.data.length>0) {
                 this.opportunities = result.data;
                 this.error = undefined;
                 this.emptyTable = undefined;
+        //S'il n'y a aucune opportunité affichée, le message d'info est affiché            
             } else {
                 this.opportunities = undefined;
                 this.error = undefined;
                 this.emptyTable = "No opportunities are associated with this account.";   
             }
+        //Si la fonction ne renvoie une erreur, un message d'erreur est affiché    
         } else {
             this.error = "An error occurred while loading opportunities."
             this.opportunities = undefined;
             this.emptyTable = undefined;
         }
     }
-
+    //permet de mettre à jour les données issues de la fonction Apex getOpportunities
     handleRefresh() {
         refreshApex(this.wiredOpportunitiesResult);
     }
